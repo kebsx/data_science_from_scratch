4b53642caeffb50004c1d79aa6fd9b2358642575
Kaleb <kalebbarnes071@gmail.com>
2025-12-17 20:42:18 -0600

    Added comment to numpy linear algebra

---
+++ linear_algebra_numpy_ver.py
@@ -0,0 +1,10 @@
+"""
+In this file I will be recreating the funcitons that we created to do linear 
+algebra, but instead of using base python objects like lists I will be using
+numpy. This will increase performance and make the functions operate smoother
+when used in machine learning applications. 
+
+Insert a quick word on Numpy here:
+"""
+
+import numpy as np
