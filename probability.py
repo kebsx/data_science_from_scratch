3ff60e39dc3483934f165b42565202a9b899c2f0
Kaleb <kalebbarnes071@gmail.com>
2025-12-19 11:42:34 -0600

    Finished all probability functions and examples

--- probability.py
+++ probability.py
@@ -87,19 +87,85 @@ plt.gca().clear()
 # erf(z) = (2/sqrt(pi)) * integral(e^-t^2) 0-z
 
 # Normal CDF
-def noraml_cdf(x: float, mu: float = 0, sigma: float = 1) -> float:
+def normal_cdf(x: float, mu: float = 0, sigma: float = 1) -> float:
     return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2
 
 xs = [x / 10.0 for x in range(-50, 50)]
 
-plt.plot(xs, [noraml_cdf(x, sigma=1) for x in xs], '-', label='mu=0, sigma=1')
-plt.plot(xs, [noraml_cdf(x, sigma=2) for x in xs], '--', label='mu=0, sigma=2')
-plt.plot(xs, [noraml_cdf(x, sigma=0.5) for x in xs], ':', label='mu=0, sigma=0.5')
-plt.plot(xs, [noraml_cdf(x, mu=-1) for x in xs], '-.', label='mu=-1, sigma=1')
+plt.plot(xs, [normal_cdf(x, sigma=1) for x in xs], '-', label='mu=0, sigma=1')
+plt.plot(xs, [normal_cdf(x, sigma=2) for x in xs], '--', label='mu=0, sigma=2')
+plt.plot(xs, [normal_cdf(x, sigma=0.5) for x in xs], ':', label='mu=0, sigma=0.5')
+plt.plot(xs, [normal_cdf(x, mu=-1) for x in xs], '-.', label='mu=-1, sigma=1')
 
 plt.legend(loc=4) # bottom right
 plt.title("Various Normal cdfs")
 # plt.show()
 
-plt.savefig("images/normal_cdfs.png")
+# plt.savefig("images/normal_cdfs.png")
 plt.gca().clear()
+
+def inverse_normal_cdf(p: float,
+                       mu: float = 0,
+                       sigma: float = 1,
+                       tolerance: float = 1e-5) -> float:
+    """Find approximate inverse using binary search"""
+
+    # if not standard, compute standard and rescale
+    if mu != 0 or sigma != 1:
+        return mu + sigma + inverse_normal_cdf(p, tolerance=tolerance)
+    
+    low_z = -10.0                       # normal_cdf(-10) is (very close to) 0
+    hi_z = 10.0                         # normal_cdf(10) is (very close to) 1
+    while hi_z - low_z > tolerance:
+        mid_z = (low_z + hi_z) / 2      # Consider the midpoint
+        mid_p = normal_cdf(mid_z)       # and the CDF's value there
+        if mid_p < p:
+            low_z = mid_z               # Midpoint too low, search above it
+        else:
+            hi_z = mid_z                # Midpoint too high, search below it
+
+    return mid_z
+
+def bernoulli_trial(p: float) -> int:
+    """Returns 1 with probability p and 0 with probability 1-p"""
+    return 1 if random.random() < p else 0
+
+def binomial(n: int, p: float) -> int:
+    """Returns the sum of n bernoulli(p) trials"""
+    return sum(bernoulli_trial(p) for _ in range(n))
+
+# The mean of a Bernoulli(p) variable is p and the std is sqrt(p(1 - p))
+# CLT says that as n gets large, Binomial(n, p) variable is approximately a
+# normal random variable with mean mu = np and std sigma = sqrt(np(1-p))
+
+from collections import Counter
+
+def binomial_histogram(p: float, n: int, num_points: int, savefig: bool) -> None:
+    """Picks points from a Binomial(n, p) and plots their histogarm"""
+    data = [binomial(n, p) for _ in range(num_points)]
+
+    # use a bar chart to show the actual binomial samples
+    histogram = Counter(data)
+    plt.bar([x - 0.4 for x in histogram.keys()],
+            [v / num_points for v in histogram.values()],
+            0.8,
+            color='0.75')
+    
+    mu = p * n
+    sigma = math.sqrt(n * p * (1 - p))
+
+    # use a line chart to show the normal approximation
+    xs = range(min(data), max(data) + 1)
+    ys = [normal_cdf(i + 0.5, mu, sigma) - normal_cdf(i - 0.5, mu, sigma)
+          for i in xs]
+    plt.plot(xs, ys)
+    plt.title("Binomial Distrubtion vs. Normal Approximation")
+    
+    if bool:
+        plt.savefig("images/binomial_dist_normal_approx.png")
+    else:
+        plt.show()
+
+    plt.gca().clear()
+
+binomial_histogram(0.75, 100, 10000, False)
