9066c2e4734d0834a4fbb9b2df1a01dd68e900f3
Kaleb <kalebbarnes071@gmail.com>
2025-12-19 11:10:00 -0600

    Added noraml cdf function and visualization

--- probability.py
+++ probability.py
@@ -76,5 +76,30 @@ plt.legend()
 plt.title("Various Normal pdfs")
 # plt.show()
 
-plt.savefig("images/normal_pdfs.png")
+# plt.savefig("images/normal_pdfs.png")
+plt.gca().clear()
+
+
+# X = sigma(X) + mu where Z is standard normal variable
+# Conversely, Z = (X - mu)/sigma when X is also a standard normal variable
+
+# For the CDF of normal dist we have to use math.erf which is 
+# erf(z) = (2/sqrt(pi)) * integral(e^-t^2) 0-z
+
+# Normal CDF
+def noraml_cdf(x: float, mu: float = 0, sigma: float = 1) -> float:
+    return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2
+
+xs = [x / 10.0 for x in range(-50, 50)]
+
+plt.plot(xs, [noraml_cdf(x, sigma=1) for x in xs], '-', label='mu=0, sigma=1')
+plt.plot(xs, [noraml_cdf(x, sigma=2) for x in xs], '--', label='mu=0, sigma=2')
+plt.plot(xs, [noraml_cdf(x, sigma=0.5) for x in xs], ':', label='mu=0, sigma=0.5')
+plt.plot(xs, [noraml_cdf(x, mu=-1) for x in xs], '-.', label='mu=-1, sigma=1')
+
+plt.legend(loc=4) # bottom right
+plt.title("Various Normal cdfs")
+# plt.show()
+
+plt.savefig("images/normal_cdfs.png")
 plt.gca().clear()
